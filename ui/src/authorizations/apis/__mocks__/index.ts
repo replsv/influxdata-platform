b47e02dd1783e1af8b4962ca1da5149725f7de8c
Gabriel <replsv@Gabriels-MacBook-Pro.local>
2022-11-05 19:26:41 +0200

    cleanup

--- ui/src/authorizations/apis/__mocks__/index.ts
+++
@@ -1,8 +0,0 @@
-import {authorization} from './data'
-
-// Types
-import {Authorization} from 'src/api'
-
-export const getAuthorizations = async (): Promise<Authorization[]> => {
-  return Promise.resolve([authorization, {...authorization, id: '1'}])
-}
